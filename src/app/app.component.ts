96b11732dc8ea4da1756c6ab8bb44725a8ba6cda
rv-2002 <raguvaranraguvaran020@gmail.com>
2024-12-02 15:16:41 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'project';
+  a:string='';
+  b:string='';
+  res:number=0;
+
+  add()
+  {
+    this.res=parseInt(this.a)+parseInt(this.b);
+    
+  }
+  sub()
+  {
+    this.res=parseInt(this.a)-parseInt(this.b)
+   
+  }
+  div()
+  {
+    this.res=parseInt(this.a)/parseInt(this.b)
+    
+  }
+  mul()
+  {
+    this.res=parseInt(this.a)*parseInt(this.b)
+    
+  }
+  clr()
+  {
+    this.a='';
+    this.b='';
+    this.res=0;
+  }
+
 }
